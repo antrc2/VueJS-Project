999e826e8b2abd347af4d7579f6400ee68c65239
antrc2 <antrc2gamer@gmail.com>
2024-11-13 00:28:41 +0700

    add lab3 and lab4

--- Labs/Lab-0/src/App.vue
+++ Labs/Lab-0/src/App.vue
@@ -1,27 +1,10 @@
 <template>
   <div>
-    <!-- <Buoi2/> -->
-     <!-- <Buoi3/> -->
-      <!-- <Bai2Buoi3/> -->
-       <!-- <DataBinding/> -->
-    <!-- <ConditionalRendering/> -->
-    <!-- <IsLogin /> -->
-     <!-- <AlertForm/> -->
-      <ListRendering />
+
   </div>
 </template>
 
 <script setup>
-import AlertForm from './components/alertForm.vue';
-import Bai2Buoi3 from './components/Bai2Buoi3.vue';
-import Buoi3 from './components/Buoi3.vue';
-import ConditionalRendering from './components/ConditionalRendering.vue';
-import DataBinding from './components/DataBinding.vue';
-import IsLogin from './components/isLogin.vue';
-import ListRendering from './components/listRendering.vue';
-
-// import Buoi2 from './components/Buoi2.vue';
-
 
 </script>
 
